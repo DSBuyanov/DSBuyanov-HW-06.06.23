755baf0c6f642773d161ee9829cb070023886dd5
DSBuyanov <inquis87@gmail.com>
2023-05-02 12:33:39 +0300

    task 2

--- lesson3/src/main/java/ru/geekbrains/lesson3/Task2.java
+++ lesson3/src/main/java/ru/geekbrains/lesson3/Task2.java
@@ -1,103 +1,45 @@
 package ru.geekbrains.lesson3;
 
-
-import java.io.IOException;
+import java.util.*;
 
 /**
-    Создайте класс Счетчик, у которого есть метод add(), увеличивающий
-    значение внутренней int переменной на 1. Сделайте так, чтобы с объектом
-    такого типа можно было работать в блоке try-with-resources. Подумайте, что
-    должно происходить при закрытии этого ресурса? Напишите метод для
-    проверки, закрыт ли ресурс. При попытке вызвать add() у закрытого
-    ресурса, должен выброситься IOException.
+ * Пусть дан список сотрудников: Иван Иванов, Светлана Петрова, Кристина Белова,
+ * Анна Мусина,
+ * Анна Крутова, Иван Юрин, Петр Лыков, Павел Чернов, Петр Чернышов, Мария
+ * Федорова, Марина Светлова,
+ * Мария Савина, Мария Рыкова, Марина Лугова, Анна Владимирова, Иван Мечников,
+ * Петр Петин, Иван Ежов.
+ * Написать программу, которая найдет и выведет повторяющиеся имена с
+ * количеством повторений.
+ * Отсортировать по убыванию популярности. Для сортировки использовать TreeMap.
  */
 
 public class Task2 {
 
-
     public static void main(String[] args) {
 
-        try(Counter counter1 = new Counter(22)) {
+        List<String> employees = Arrays.asList("Иван Иванов", "Светлана Петрова", "Кристина Белова", "Анна Мусина",
+                "Анна Крутова", "Иван Юрин", "Петр Лыков", "Павел Чернов", "Петр Чернышов", "Мария Федорова",
+                "Марина Светлова", "Мария Савина", "Мария Рыкова", "Марина Лугова", "Анна Владимирова", "Иван Мечников",
+                "Петр Петин", "Иван Ежов");
 
-            System.out.printf("Текущее значение счетчика %d\n", counter1.add());
+        Map<String, Integer> countMap = new HashMap<>();
 
-            counter1.add();
-            counter1.add();
-
-            System.out.printf("Текущее значение счетчика %d\n", counter1.getCounter());
-            //counter.closeCounter();
-        }
-        catch (CloseCounterException e){
-            System.out.println(e.getMessage());
-        }
-        catch (CounterException e){
-
-        }
-        catch (CounterV2Exception e){
-
+        for (String employee : employees) {
+            String name = employee.split(" ")[0];
+            countMap.compute(name, (k, v) -> (v == null) ? 1 : v + 1);
         }
 
-    }
+        TreeMap<Integer, List<String>> sortedMap = new TreeMap<>(Collections.reverseOrder());
 
-}
+        countMap.forEach((name, count) -> {
+            List<String> list = sortedMap.getOrDefault(count, new ArrayList<>());
+            list.add(name);
+            sortedMap.put(count, list);
+        });
 
-class Counter implements AutoCloseable{
-
-    private int counter;
-    private boolean isOpen;
-
-    public int getCounter() {
-        return counter;
-    }
-
-
-    /**
-     *
-     * @return
-     * @throws CloseCounterException исключение 1
-     * @throws CounterException исключение 2
-     * @throws CounterV2Exception исключение 3
-     */
-    public int add() throws CloseCounterException, CounterException, CounterV2Exception{
-        if (!isOpen){
-            throw new CloseCounterException("Счетчик закрыт.");
-        }
-        return ++counter;
-    }
-
-    public Counter(){
-        isOpen = true;
-    }
-
-    public Counter(int counter){
-        this.counter = counter;
-        isOpen = true;
-    }
-
-    public void closeCounter(){
-        isOpen = false;
-    }
-
-    @Override
-    public void close() {
-        closeCounter();
-    }
-}
-
-class CloseCounterException extends IOException{
-    public CloseCounterException(String message) {
-        super(message);
-    }
-}
-
-class CounterException extends Exception{
-    public CounterException(String message) {
-        super(message);
-    }
-}
-
-class CounterV2Exception extends RuntimeException{
-    public CounterV2Exception(String message) {
-        super(message);
+        sortedMap.forEach((count, names) -> {
+            System.out.println(names + ": " + count);
+        });
     }
 }
